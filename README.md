a32c382668ddf4b9c4f329ba6583faa8915e86bf
matthew <matthkang@gmail.com>
2023-07-25 18:08:59 -0700

    history and delete button

--- README.md
+++ README.md
@@ -21,8 +21,15 @@
 
 
 ## Usage
+Weather application displaying the <ins>current</ins> and the <ins>next 5 days</ins> forecast
 
+![Weather Dashboard](assets/images/weather-dashboard.png)
+
+- You can search for a city in the search bar. 
+- Searching for a city will add it to list of recent searches to retrieve the weather forecast again.
 
 ## Credits
 - https://stackoverflow.com/questions/3304014/how-to-interpolate-variables-in-strings-in-javascript-without-concatenation
 - https://stackoverflow.com/questions/17634019/javascript-load-an-image-from-url-and-display
+- https://stackoverflow.com/questions/4323848/how-to-handle-button-click-events-in-jquery
+- https://www.freecodecamp.org/news/javascript-refresh-page-how-to-reload-a-page-in-js/#:~:text=The%20simplest%20way%20to%20refresh,and%20loading%20the%20latest%20content.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var cityFormEl = document.querySelector('#city-form');
 var cityInputEl = document.querySelector('#cityInput');
+var cityHistoryButton = $('#city-history');
 
 var presentForecast = document.querySelector('#present-forecast');
 var futureForecast = document.querySelector('#future-forecast');
@@ -9,27 +10,67 @@ var currentTemp = document.querySelector('#temp');
 var currentWind = document.querySelector('#wind');
 var currentHumidity = document.querySelector('#humidity');
 
+// objs to store in local storage
+// var currentWeatherObj;
+// var futureWeatherObj;
+
 var city;
 
 var todayDate = dayjs().format('MM/DD/YYYY');
 
 var apiKey = '6d16b9f1f20092ca89c4382c6758f30c';
 
+// listener for search button
 var formSubmitHandler = function (event) {
     event.preventDefault();
 
     city = cityInputEl.value.trim();
 
     if (city) {
+        saveLocalStorage(city);
         getCoords(city);
+        renderHistory();
 
         presentForecast.textContent = '';
-        
+        futureForecast.textContent = '';
+
         cityInputEl.value = '';
     } else {
         alert('Please enter a city');
     }
 };
+
+function saveLocalStorage(city) {
+    // add city to localStorage
+    var cityStorage = localStorage.getItem("cities");
+
+    // retrieve arr from localStorage or create new arr
+    if (cityStorage !== null) {
+        var citiesArr = JSON.parse(cityStorage);
+    }
+    else {
+        var citiesArr = [];
+    }
+
+    let exists = false;
+
+    // check if city exists already in localStorage
+    for (var i = 0; i < citiesArr.length; i++) {
+        // if city already exists
+        if (citiesArr.includes(city)) {
+            exists = true;
+        }
+    }
+
+    // first time searching city, push city to array
+    if (exists === false) {
+        citiesArr.push(city);
+    }
+
+    localStorage.setItem("cities", JSON.stringify(citiesArr));
+}
+
+
 
 // get coords from city input using geocoding API
 var getCoords = function (city) {
@@ -40,9 +81,9 @@ var getCoords = function (city) {
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
-                console.log("response: ", response);
+                // console.log("response: ", response);
                 response.json().then(function (data) {
-                    console.log("data: ", data);
+                    // console.log("data: ", data);
                     var lat = data[0].lat;
                     var lon = data[0].lon;
 
@@ -65,9 +106,9 @@ var getCurrentWeather = function (lat, lon) {
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
-                console.log(response);
+                // console.log(response);
                 response.json().then(function (data) {
-                    console.log("current weather: ", data);
+                    // console.log("current weather: ", data);
                     displayCurrentWeather(data);
                 });
             } else {
@@ -84,7 +125,8 @@ var displayCurrentWeather = function (data) {
     var date = ' (' + todayDate + ')';
     var h3City = document.createElement('h3');
     h3City.setAttribute('id', 'city');
-    h3City.textContent += ("City: " + city + date);
+    city += date;
+    h3City.textContent += (city);
 
     var val = data.weather[0].icon,
         src = 'https://openweathermap.org/img/wn/' + val + '@2x.png',
@@ -99,6 +141,16 @@ var displayCurrentWeather = function (data) {
     var wind = data.wind.speed;
     var humidity = data.main.humidity;
 
+    // currentWeatherObj = {
+    //     city: city,
+    //     weatherIcon: weatherIcon,
+    //     temp: temp,
+    //     wind: wind,
+    //     humidity: humidity,
+    // };
+
+    // console.log(currentWeatherObj);
+
     var h3Temp = document.createElement('h3');
     h3Temp.setAttribute('id', 'temp');
 
@@ -107,7 +159,7 @@ var displayCurrentWeather = function (data) {
 
     var h3Humidity = document.createElement('h3');
     h3Humidity.setAttribute('id', 'humidity');
-    
+
 
     h3Temp.textContent = ("Temp: " + temp + ' °F');
     h3Wind.textContent = ("Wind: " + wind + ' MPH');
@@ -126,9 +178,9 @@ var get5DayWeather = function (lat, lon) {
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
-                console.log(response);
+                // console.log(response);
                 response.json().then(function (data) {
-                    console.log("5 day forecast: ", data);
+                    // console.log("5 day forecast: ", data);
                     display5DayForecast(data);
                 });
             } else {
@@ -144,14 +196,14 @@ var display5DayForecast = function (data) {
     var row = document.createElement('div');
     row.classList = 'row';
 
-    for (var i = 1; i < 6; i++){
+    for (var i = 1; i < 6; i++) {
         var col = document.createElement('div');
         col.classList = 'col card';
 
         // add date for next 5 days
         var h5El = document.createElement('h5');
         h5El.setAttribute('id', 'date-' + i);
-        h5El.classList = 'card-header text-uppercase';
+        h5El.classList = 'card-header text-uppercase lightblue-bg';
 
         var date = dayjs().add(i, 'day').format('MM/DD/YYYY');
         h5El.textContent = date;
@@ -160,11 +212,11 @@ var display5DayForecast = function (data) {
         var icon = document.createElement('span');
         icon.setAttribute('id', 'icon-' + i);
 
-        var ind = (i-1)*8;
+        var ind = (i - 1) * 8;
         var dataIndex = data.list[ind];
         var val = dataIndex.weather[0].icon,
-        src = 'https://openweathermap.org/img/wn/' + val + '@2x.png',
-        img = document.createElement('img');
+            src = 'https://openweathermap.org/img/wn/' + val + '@2x.png',
+            img = document.createElement('img');
 
         img.src = src;
         var weatherIcon = img;
@@ -173,6 +225,15 @@ var display5DayForecast = function (data) {
         var tempVal = dataIndex.main.temp;
         var windVal = dataIndex.wind.speed;
         var humidityVal = dataIndex.main.humidity;
+
+        // futureWeatherObj = {
+        //     date: date,
+        //     weatherIcon: weatherIcon,
+        //     temp: tempVal,
+        //     wind: windVal,
+        //     humidity: humidityVal,
+        // }
+        // console.log("futureWeatherObj: ", futureWeatherObj);
 
         // temperature
         var temp = document.createElement('h6');
@@ -197,5 +258,93 @@ var display5DayForecast = function (data) {
 
     futureForecast.appendChild(row);
 };
+
+function renderHistory() {
+    var cityStorage = localStorage.getItem("cities");
+    var citiesArr = JSON.parse(cityStorage);
+    // retrieve div for id of recent-searches
+    var recentSearchesEl = document.querySelector('#recent-searches');
+
+    // clear history element first before adding buttons
+    recentSearchesEl.innerHTML = "";
+
+    // adding buttons for searched cities
+    if (cityStorage !== null) {
+        for (var i = 0; i < citiesArr.length; i++) {
+            var containerDiv = document.createElement('div');
+            containerDiv.classList = "container";
+
+            var historyDiv = document.createElement('div');
+            historyDiv.classList = "row history-div";
+
+            var btnDiv = document.createElement('div');
+            btnDiv.classList = "col-9";
+
+            var btnDeleteDiv = document.createElement('div');
+            btnDeleteDiv.classList = "col-3";
+
+            var historyBtn = document.createElement('button');
+            historyBtn.classList = "btn btn-secondary";
+            historyBtn.textContent = citiesArr[i];
+
+            var deleteBtn = document.createElement('button');
+            deleteBtn.classList = "btn btn-danger";
+            deleteBtn.textContent = "X";
+
+            containerDiv.appendChild(historyDiv);
+            btnDiv.appendChild(historyBtn);
+
+            btnDeleteDiv.appendChild(deleteBtn);
+
+            historyDiv.appendChild(btnDiv);
+            historyDiv.appendChild(btnDeleteDiv);
+
+            recentSearchesEl.appendChild(containerDiv);
+        }
+    }
+}
+
+$(document).ready(function() {
+    var historyButton = $(".btn-secondary");
+    var deleteButton = $(".btn-danger");
+
+    historyButton.click(function(event){
+        var cityHistory = event.target.textContent;
+        city = cityHistory;
+
+        // clear current and 5-day forecast
+        presentForecast.textContent = '';
+        futureForecast.textContent = '';
+
+        getCoords(cityHistory);
+    });
+
+    deleteButton.click(function(event){
+        var cityToDelete = $(this).parent().parent().children()[0].textContent;
+        var cityStorage = localStorage.getItem("cities");
+        var citiesArr = JSON.parse(cityStorage);
+
+        // find index of element to remove
+        const index = citiesArr.indexOf(cityToDelete);
+
+        // delete if item is found
+        if (index > -1){
+            // 2nd parameter to remove one item only
+            citiesArr.splice(index, 1);
+        }
+
+        // update localStorage after deleting entry from array
+        localStorage.setItem("cities", JSON.stringify(citiesArr));
+        renderHistory();
+        location.reload();
+    });
+});
+
+
+// The init() function fires when the page is loaded 
+function init() {
+    renderHistory();
+}
+init();
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
